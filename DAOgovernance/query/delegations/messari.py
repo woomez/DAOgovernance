e3fdcf0ca08f49eb139c876c593563d0d5781b10
Jaeyong <jaeyong2328@gmail.com>
2022-09-29 02:20:29 -0700

    adding angle/fei/euler

--- DAOgovernance/query/delegations/messari.py
+++ DAOgovernance/query/delegations/messari.py
@@ -2,18 +2,10 @@ from queries import query
 import pandas as pd
 import gc
 
-urls = [('Compound', 'https://api.thegraph.com/subgraphs/name/danielkhoo/compound-governance'),
-        ('Dydx', 'https://api.thegraph.com/subgraphs/name/messari/dydx-governance'),
-        ('Uniswap', 'https://api.thegraph.com/subgraphs/name/danielkhoo/uniswap-governance'),
-        ('Aave', 'https://api.thegraph.com/subgraphs/name/messari/aave-governance'),
-        ('Hop', 'https://api.thegraph.com/subgraphs/name/messari/hop-governance'),
-        ('Truefi', 'https://api.thegraph.com/subgraphs/name/messari/truefi-governance'),
-        ('Silo', 'https://api.thegraph.com/subgraphs/name/messari/silo-governance'),
-        ('ENS', 'https://api.thegraph.com/subgraphs/name/messari/ens-governance'),
-        ('Code4rena', 'https://api.thegraph.com/subgraphs/name/danielkhoo/code4rena-governance'),
-        ('Fei', 'https://api.thegraph.com/subgraphs/name/messari/fei-governance'),
-        ('Unlock', 'https://api.thegraph.com/subgraphs/name/messari/unlock-governance'),
-        ('Euler', 'https://api.thegraph.com/subgraphs/name/messari/euler-governance')
+urls = [('Angle', 'https://api.studio.thegraph.com/query/28876/angle-governance/v2'),
+        ('ENS', 'https://api.studio.thegraph.com/query/28876/ens-governance/v5'),
+        ('Fei', 'https://api.studio.thegraph.com/query/28876/fei-governance/v1'),
+        ('Euler', 'https://api.studio.thegraph.com/query/28876/euler-governance/v2')
         ]
 
 _queries = ["delegates", "governances", "proposals", "votes", "delegations", "tokendailysnapshots", "votedailysnapshots"]
@@ -23,8 +15,9 @@ def generate_results(url, _queries):
     print(f"starting process for {dao}")
     for _query in _queries:
         query(dao, api, _query)
-    gc.collect()
+        gc.collect()
 
 ens_url = ('ENS', 'https://api.studio.thegraph.com/query/28876/ens-governance/v5')
 
-generate_results(ens_url, _queries)
+for url in urls:
+    generate_results(url, _queries)
