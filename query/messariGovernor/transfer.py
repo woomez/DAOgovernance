08d8c22fa67ee3dbcc3223951c718f5f0de2a664
Jaeyong <jaeyong2328@gmail.com>
2023-04-26 19:09:19 -0700

    restructure

--- query/messariGovernor/transfer.py
+++ query/messariGovernor/transfer.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from web3 import Web3
 from dotenv import load_dotenv
 import json
@@ -8,7 +9,13 @@ from tqdm import tqdm
 
 load_dotenv()
 
-with open("./openzepplinGovernor/abis/transfer.json", "r") as abi_file:
+parser = argparse.ArgumentParser(description='Process transfers from specified folder.')
+parser.add_argument('--folder', type=str, default='openzepplinGovernor', help='Folder name to load the data from (default: openzepplinGovernor)')
+
+args = parser.parse_args()
+folder_name = args.folder
+
+with open(f"./messariGovernor/{folder_name}/abis/transfer.json", "r") as abi_file:
     token_abi = json.load(abi_file)
 
 infura_api_key = os.getenv('INFURA_API_KEY')
@@ -16,21 +23,24 @@ infura_url = f'https://mainnet.infura.io/v3/{infura_api_key}'
 w3 = Web3(Web3.HTTPProvider(infura_url)) 
 
 # Replace with the transaction hash you're interested in
-with open('./openzepplinGovernor/dao.json', 'r') as f:
+with open('./messariGovernor/{folder_name}/dao.json', 'r') as f:
     dao = json.load(f)
 
 if not os.path.exists('./res/transfer'):
     os.makedirs('./res/transfer')
 
 for name in dao.keys():
-
     # create df to store results
     results = pd.DataFrame(columns=['txnHash', 'from', 'to', 'value'])
     print(name)
     values = dao[name]
     token_address = Web3.to_checksum_address(values['token'])
-    # load delegation csv
-    df = pd.read_csv(f'./res/delegateVotingPowerChanges/{name}.csv')
+    # load delegation csv if there is one
+    delegation_path = f'./res/delegateVotingPowerChanges/{name}.csv'
+    if os.path.exists(delegation_path):
+        df = pd.read_csv(delegation_path)
+    else: #stop loop
+        continue
     print(f'{name} size: {len(df)}')
     # if transfer csv exist, load it and fetch the last txnHash
     if os.path.exists(f'./res/transfer/{name}.csv'):
